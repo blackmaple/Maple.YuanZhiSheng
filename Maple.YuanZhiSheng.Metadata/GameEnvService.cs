6cb4f9da42a32af68cf95d92a4ce168d8248ea3b
红色枫叶 <BlackMaple@live.cn>
2026-06-19 20:32:02 +0800

    1

--- Maple.YuanZhiSheng.Metadata/GameEnvService.cs
+++ Maple.YuanZhiSheng.Metadata/GameEnvService.cs
@@ -1,7 +1,8 @@
 ﻿namespace Maple.YuanZhiSheng.Metadata
 {
-    public readonly partial struct GameEnvService
+    public readonly partial struct GameEnvService(GameResourceCache cache)
     {
-
+        GameResourceCache Cache { get; } = cache;   
+        
     }
 }
